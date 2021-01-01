cefcafc0535164c90d48ddaf99af4e368e3d5e3a
aalku <aalku7@gmail.com>
2021-01-01 13:47:54 +0100

    Update PixelMatrixFun-ESP32.ino
    Progress

--- PixelMatrixFun-ESP32/PixelMatrixFun-ESP32.ino
+++ PixelMatrixFun-ESP32/PixelMatrixFun-ESP32.ino
@@ -14,10 +14,13 @@ BLECharacteristic * pTxCharacteristic;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
 uint8_t txValue[7] = "HELO\r\n";
+int lastClearSeq = -1;
 
-#define SERVICE_UUID                   "6ff4913c-ea8a-4e5b-afdc-9f0f0e488ab1"
-#define CHARACTERISTIC_UUID_RGB_BITMAP "6ff4913c-ea8a-4e5b-afdc-9f0f0e488ab2"
-#define CHARACTERISTIC_UUID_HELO       "6ff4913c-ea8a-4e5b-afdc-9f0f0e488ab3"
+#define SERVICE_UUID                    "5e92c674-3f45-420c-bb53-af2bcaff68b2"
+#define CHARACTERISTIC_UUID_RGB_BITMAP  "2de8d042-9a37-45f0-8233-5160faea472d"
+#define CHARACTERISTIC_UUID_HELO        "3ca58a89-0754-44d3-83e5-371aac29ca36"
+#define CHARACTERISTIC_UUID_PIXEL_WRITE "5038a8b8-5e8e-4f1f-97dd-8cf76c8e90e1"
+#define CHARACTERISTIC_UUID_CLEAR       "2ae4ad40-f1e2-4f4d-893c-f0109c024381"
 
 // How many leds in your strip?
 #define NUM_LEDS 256
@@ -58,7 +61,7 @@ void drawFromFlash() {
       unsigned char r=EEPROM.read((x*16+y)*3);
       unsigned char g=EEPROM.read((x*16+y)*3+1);
       unsigned char b=EEPROM.read((x*16+y)*3+2);
-      Serial.print("[");Serial.print(x);Serial.print(",");Serial.print(y);Serial.print("]=[");Serial.print(r);Serial.print(",");Serial.print(g);Serial.print(",");Serial.print(b);Serial.println("]");
+      // Serial.print("[");Serial.print(x);Serial.print(",");Serial.print(y);Serial.print("]=[");Serial.print(r);Serial.print(",");Serial.print(g);Serial.print(",");Serial.print(b);Serial.println("]");
       unsigned long pixPos;
       if (x % 2 == 0) {
         pixPos = x*16+y;
@@ -83,7 +86,7 @@ void drawFromFlash() {
 }
 
 
-class MyCallbacks: public BLECharacteristicCallbacks {
+class BitmapWriteCallback: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
       std::string rxValue = pCharacteristic->getValue();
       if (rxValue.length() > 0) {
@@ -105,6 +108,58 @@ class MyCallbacks: public BLECharacteristicCallbacks {
       if (receiveOffset >= 16*16*3) {
         drawFromFlash();
         receiveOffset=0;
+      }
+    }
+};
+
+class PixelWriteCallback: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string rxValue = pCharacteristic->getValue();
+      if (rxValue.length() > 0) {
+        for (int i = 0; i < rxValue.length(); i += 5) {
+          unsigned char x = rxValue[i];
+          unsigned char y = rxValue[i+1];
+          unsigned char r = rxValue[i+2];
+          unsigned char g = rxValue[i+3];
+          unsigned char b = rxValue[i+4];
+          Serial.print("[");Serial.print(x);Serial.print(",");Serial.print(y);Serial.print("]=[");Serial.print(r);Serial.print(",");Serial.print(g);Serial.print(",");Serial.print(b);Serial.print("], totalBytes=");Serial.println(rxValue.length());
+          EEPROM.write((x*16+y)*3+0, r);
+          EEPROM.write((x*16+y)*3+1, g);
+          EEPROM.write((x*16+y)*3+2, b);
+        }
+        EEPROM.commit();
+        drawFromFlash();
+      }
+    }
+};
+
+class ClearCallback: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      // if (!pCharacteristic->getUUID().equals(BLEUUID::fromString(CHARACTERISTIC_UUID_CLEAR))) {
+      //  Serial.println("UUID Mistake!");
+      //}
+      std::string rxValue = pCharacteristic->getValue();
+      if (rxValue.length() >= 4) {
+        unsigned char r = rxValue[0];
+        unsigned char g = rxValue[1];
+        unsigned char b = rxValue[2];
+        unsigned char s = rxValue[3];
+        if (s == lastClearSeq) {
+          Serial.println("Repeated clear received");
+          return;
+        } else {
+          lastClearSeq = s;
+        }
+        Serial.print("[fill]=[");Serial.print(r);Serial.print(",");Serial.print(g);Serial.print(",");Serial.print(b);Serial.print("], totalBytes=");Serial.println(rxValue.length());
+        for (int x = 0; x < 16; x++) {
+          for (int y = 0; y < 16; y++) {
+            EEPROM.write((x*16+y)*3+0, r);
+            EEPROM.write((x*16+y)*3+1, g);
+            EEPROM.write((x*16+y)*3+2, b);
+          }
+        }
+        EEPROM.commit();
+        drawFromFlash();
       }
     }
 };
@@ -151,12 +206,23 @@ void startService() {
                       
   pTxCharacteristic->addDescriptor(new BLE2902());
 
-  BLECharacteristic * pRxCharacteristic = pService->createCharacteristic(
+  BLECharacteristic * pBitmapCharacteristic = pService->createCharacteristic(
                        CHARACTERISTIC_UUID_RGB_BITMAP,
                       BLECharacteristic::PROPERTY_WRITE
                     );
+  pBitmapCharacteristic->setCallbacks(new BitmapWriteCallback());
 
-  pRxCharacteristic->setCallbacks(new MyCallbacks());
+  BLECharacteristic * pPixelCharacteristic = pService->createCharacteristic(
+                       CHARACTERISTIC_UUID_PIXEL_WRITE,
+                      BLECharacteristic::PROPERTY_WRITE
+                    );
+  pPixelCharacteristic->setCallbacks(new PixelWriteCallback());
+
+  BLECharacteristic * pClearCharacteristic = pService->createCharacteristic(
+                       CHARACTERISTIC_UUID_CLEAR,
+                      BLECharacteristic::PROPERTY_WRITE
+                    );
+  pClearCharacteristic->setCallbacks(new ClearCallback());
 
   // Start the service
   pService->start();
